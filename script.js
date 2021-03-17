83a33b830846d7f7bf5efa1a409a370cad380164
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-03-18 03:50:25 +0600

    Add velocity to the snake

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ let tileCount = 20;
 let tileSize = canvas.width / tileCount - 2;
 let headX = 10;
 let headY = 10;
-
+let xVelocity = 0;
+let yVelocity = 0;
 
 function drawGame() {
+    chnageSnakePosition();
     clearScreen();
     drawSnake();
     
@@ -20,9 +22,50 @@ const clearScreen = () => {
     ctx.fillRect(0,0,canvas.width,canvas.height);
 };
 
-function drawSnake() {
+const drawSnake = () => {
     ctx.fillStyle =  'orange';
     ctx.fillRect(headX * tileCount, headY* tileCount, tileSize,tileSize);
 }
+
+const chnageSnakePosition = () => {
+    headX = headX + xVelocity;
+    headY = headY + yVelocity;
+}
+
+
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown(event){
+    //up
+    if(event.keyCode == 38){
+        
+        yVelocity = -1;
+        xVelocity = 0;
+    }
+
+    if(event.keyCode == 40){
+        
+        yVelocity = 1;
+        xVelocity = 0;
+    }
+
+    if(event.keyCode == 37){
+        if(xVelocity == 1)
+            return;
+        yVelocity = 0;
+        xVelocity = -1;
+    }
+
+    if(event.keyCode == 39){
+        if(xVelocity == -1)
+        return;
+        yVelocity = 0;
+        xVelocity = 1;
+    }
+
+
+
+}
+
 
 drawGame();
