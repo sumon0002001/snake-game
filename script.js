f105571105cd5a113adc05cdebbc87e177d5ebfe
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-03-18 02:33:02 +0600

    Draw board

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const canvas = document.getElementById('game')
-constctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 let speed = 7;
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+let headX = 10;
+let headY = 10;
+
 
 function drawGame() {
- 
+
+    clearScreen();
     setTimeout(drawGame, 100/speed);
 }
+
+const clearScreen = () => {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+};
 
 drawGame();
